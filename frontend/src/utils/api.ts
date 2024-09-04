30b16e834ba28b4394c5d1900d0a3bb48b47f355
meeehdi <mehdi.mhiri@gmail.com>
2024-09-04 09:22:20 +0200

    feat(api): force revalidate to 0 on build

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -3,9 +3,17 @@ export async function fetchApi<T>(
   init?: RequestInit,
 ): Promise<T | undefined> {
   try {
-    // TODO: force revalidate 0 at build time
+    if (process.env.NEXT_PHASE == "phase-production-build") {
+      if (!init) {
+        init = {};
+      }
+      if (!init.next) {
+        init.next = {};
+      }
+      init = { ...init, next: { ...init.next, revalidate: 0 } };
+    }
+
     const res = await fetch(`${process.env.API_URL}${input}`, init);
-    console.log(res.headers.get("content-type"))
     if (res.ok) {
       if (res.headers.get("content-type")?.startsWith("application/json")) {
         return res.json() as T;
