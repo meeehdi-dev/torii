bea09b1ce7d3812b33e437c142b56a5272310c68
meeehdi <mehdi.mhiri@gmail.com>
2024-09-04 09:21:30 +0200

    feat(ping): add timer

--- frontend/src/components/ping.tsx
+++ frontend/src/components/ping.tsx
@@ -2,11 +2,15 @@ import { fetchApi } from "@/utils/api";
 import { FC } from "react";
 
 export const Ping: FC = async () => {
+  const start = new Date();
   const pong = await fetchApi<string>("/ping", {
     next: { revalidate: 10 },
   });
+  const end = new Date();
 
-  console.log(pong);
-
-  return <div>ping: {pong}</div>;
+  return (
+    <div>
+      ping: {pong} ({end.valueOf() - start.valueOf()} ms)
+    </div>
+  );
 };
