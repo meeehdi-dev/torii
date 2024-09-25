78db6ac9046ce21b2a564e99aa9da3128026de23
meeehdi <mehdi.mhiri@gmail.com>
2024-09-25 21:21:09 +0200

    feat(remix): remove project, meh

--- frontend-remix/app/components/ping.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from "react";
-
-interface PingProps {
-  pong: string;
-}
-
-export const Ping: FC<PingProps> = ({ pong }) => {
-  return <div>ping | {pong}</div>;
-};
--- frontend-remix/app/sessions.ts
+++
@@ -1,9 +0,0 @@
-import { createCookie, createCookieSessionStorage } from "@remix-run/node"; // or cloudflare/deno
-
-type SessionData = {
-  uuid: string;
-};
-
-export const userSession = createCookieSessionStorage<SessionData>({
-  cookie: createCookie("remix_session"),
-});
